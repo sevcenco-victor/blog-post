b5a6382b16b6a43ee0bfbe384b9048817bddf959
Victor Sevcenco <sevcenco.dev@gmail.com>
2024-12-28 16:13:13 +0200

    Updated Database entity from Blog to Post

--- backend/BlogPost.Api/BlogPost.Infrastructure/Data/BlogDbContext.cs
+++ backend/BlogPost.Api/BlogPost.Infrastructure/Data/BlogDbContext.cs
@@ -1,20 +1,21 @@
 using BlogPost.Domain.Entities;
-using BlogPost.Infrastructure.Data.Configurations;
+using BlogPost.Infrastructure.Configurations;
 using Microsoft.EntityFrameworkCore;
+using PostEntity = BlogPost.Domain.Entities.Post;
 
 namespace BlogPost.Infrastructure.Data;
 
 public class BlogDbContext(DbContextOptions<BlogDbContext> options)
     : DbContext(options)
 {
-    public DbSet<Blog> Blogs => Set<Blog>();
+    public DbSet<PostEntity> Posts => Set<PostEntity>();
     public DbSet<Tag> Tags => Set<Tag>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        
-        modelBuilder.ApplyConfiguration(new BlogConfiguration());
+
+        modelBuilder.ApplyConfiguration(new PostConfiguration());
         modelBuilder.ApplyConfiguration(new TagConfiguration());
     }
 }
---
+++ backend/BlogPost.Api/BlogPost.Infrastructure/Data/Migrations/20241227115826_SetDefaultValueForPostDate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogPost.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetDefaultValueForPostDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "PostDate",
+                table: "Blogs",
+                type: "date",
+                nullable: false,
+                defaultValueSql: "CURRENT_DATE",
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "PostDate",
+                table: "Blogs",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldDefaultValueSql: "CURRENT_DATE");
+        }
+    }
+}
