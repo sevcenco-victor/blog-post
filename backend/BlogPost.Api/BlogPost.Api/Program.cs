e4f82048edc1fca8d23b677eabf25e3070b76d87
Victor Sevcenco <sevcenco.dev@gmail.com>
2024-12-28 16:36:08 +0200

    App start class

--- backend/BlogPost.Api/BlogPost.Api/Program.cs
+++ backend/BlogPost.Api/BlogPost.Api/Program.cs
@@ -1,14 +1,26 @@
-using BlogPost.Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
+using BlogPost.Api.Extensions;
+using BlogPost.Application;
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<BlogDbContext>(options => 
-    options.UseNpgsql(builder.Configuration.GetConnectionString("BlogDbConnection")));
+builder.Services.AddServices(builder.Configuration);
+builder.Services.AddApplication();
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly, includeInternalTypes: true);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
