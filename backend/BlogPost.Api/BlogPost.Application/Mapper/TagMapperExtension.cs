ee9b6c8bfd541d9fbf6d303b4efd6606ba9f98fa
Victor Sevcenco <sevcenco.dev@gmail.com>
2024-12-29 12:13:01 +0200

    included color to mapping

--- backend/BlogPost.Api/BlogPost.Application/Mapper/TagMapperExtension.cs
+++ backend/BlogPost.Api/BlogPost.Application/Mapper/TagMapperExtension.cs
@@ -1,5 +1,4 @@
 using BlogPost.Application.Contracts.Tag;
-using BlogPost.Domain.Contracts.Tag;
 using BlogPost.Domain.Entities;
 
 namespace BlogPost.Application.Mapper;
@@ -11,12 +10,13 @@ public static class TagMapperExtension
         return new Tag()
         {
             Name = tagRequest.Name,
+            Color = tagRequest.Color
         };
     }
 
     public static TagResponse ToTagResponse(this Tag tag)
     {
-        return new TagResponse(tag.Id, tag.Name);
+        return new TagResponse(tag.Id, tag.Name, tag.Color);
     }
 
     public static Tag ToEntity(this UpdateTagRequest tagRequest)
@@ -24,6 +24,7 @@ public static class TagMapperExtension
         return new Tag()
         {
             Name = tagRequest.Name,
+            Color = tagRequest.Color
         };
     }
 }
