6e115042c56f89839e13ed692cbe79ef359a92df
Victor Sevcenco <sevcenco.dev@gmail.com>
2024-12-29 12:09:29 +0200

    Added Color Prop

--- backend/BlogPost.Api/BlogPost.Domain/Entities/Tag.cs
+++ backend/BlogPost.Api/BlogPost.Domain/Entities/Tag.cs
@@ -6,5 +6,6 @@ namespace BlogPost.Domain.Entities;
 public class Tag : Entity
 {
     public string Name { get; set; } = string.Empty;
+    public string Color { get; set; } = string.Empty;
     [JsonIgnore] public IEnumerable<Post> Blogs { get; set; } = new List<Post>();
 }
