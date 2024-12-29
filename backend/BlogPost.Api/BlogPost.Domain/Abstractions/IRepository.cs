bb472c2e745b565626bac141436fb174535e7c2e
Victor Sevcenco <sevcenco.dev@gmail.com>
2024-12-29 12:09:03 +0200

    Changed Update method params

--- backend/BlogPost.Api/BlogPost.Domain/Abstractions/IRepository.cs
+++ backend/BlogPost.Api/BlogPost.Domain/Abstractions/IRepository.cs
@@ -5,6 +5,6 @@ public interface IRepository<TK, T> where T : class
     Task<int> CreateAsync(T entity);
     Task<T?> GetByIdAsync(TK id);
     Task<IEnumerable<T>> GetAllAsync();
-    Task<bool> UpdateAsync(T entity, TK entityId);
+    Task<bool> UpdateAsync(T entity);
     Task<bool> DeleteAsync(TK id);
 }
