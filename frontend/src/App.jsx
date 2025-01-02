8cd18b638e88e9e6783d915800f5ebd8842618ff
Victor Sevcenco <sevcenco.dev@gmail.com>
2025-01-02 23:05:44 +0200

    add application structure and theme support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,23 @@
-function App() {
+import NavBar from "./components/NavBar/NavBar.jsx";
+import MainContent from "./components/MainContent/MainContent.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import {useState} from "react";
+import './styles/styles.css';
 
+function App() {
+    const userPreference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const [theme, setTheme] = useState(userPreference ? "dark" : "light");
+    document.documentElement.setAttribute("data-theme", theme);
+
+    return (
+        <div className={'app'}>
+            <NavBar theme={theme} setTheme={setTheme}/>
+            <div className={'container'}>
+                <MainContent/>
+                <Footer/>
+            </div>
+        </div>
+    )
 }
 
 export default App
