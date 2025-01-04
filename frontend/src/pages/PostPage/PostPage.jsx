9d1184daf954354dfc39be2d97c1cc54e7084c3a
Victor Sevcenco <sevcenco.dev@gmail.com>
2025-01-05 00:25:30 +0200

    add PostPage fetch

--- frontend/src/pages/PostPage/PostPage.jsx
+++ frontend/src/pages/PostPage/PostPage.jsx
@@ -1,122 +1,57 @@
+import {useEffect, useState} from "react";
 import {useParams} from "react-router";
+import gfm from 'remark-gfm'
+import Markdown from 'react-markdown';
 import PostCard from "src/components/Cards/PostCard/PostCard.jsx";
 import NewsletterSubscription from "src/components/NewsletterSubscription/NewsletterSubscription.jsx";
-import styles from './PostPage.module.css'
-
-const tagList = [
-    {name: "Design", color: "#C11574"},
-    {name: "Development", color: "#007ACC"},
-    {name: "Marketing", color: "#F7931A"},
-    {name: "Finance", color: "#2ECC71"},
-    {name: "HR", color: "#9B59B6"}
-];
-
-const postCards = [
-    {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    }, {
-        imageUrl: 'https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg',
-        postDate: "postDate",
-        title: "Migrating to Linear 101",
-        text: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-        tagList: tagList,
-        orientation: "portrait"
-    },
-
-]
+import useAxios from "src/hooks/useAxios.jsx";
+import axiosInstance from 'src/apis/axiosInstance.jsx';
+import {mockPosts} from "src/mock/mockPosts.js";
+import 'react-markdown-editor-lite/lib/index.css';
+import styles from './PostPage.module.scss';
 
 const PostPage = () => {
     const {postId} = useParams();
+    const [markdown, setMarkdown] = useState("");
+    const {response, error, loading} = useAxios({
+            axiosInstance,
+            url: `/post/${postId}`,
+            method: 'get'
+        }
+    )
+
+    useEffect(() => {
+        const fetchMarkdown = async () => {
+            try {
+                const res = await fetch(response.markDownFileLink);
+                if (!res.ok) {
+                    throw new Error("Failed to fetch the Markdown file.");
+                }
+                const text = await res.text();
+                setMarkdown(text);
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        if (response) {
+            fetchMarkdown();
+        }
+    }, [response]);
+
+    if (!markdown || loading) return <p>Loading...</p>;
+
+    if (error) {
+        return <p>{error}</p>
+    }
 
     return (
-        <div className={`page ${styles['post-page']}`}>
+        <div className={`${styles['post-page']}`}>
             <section className={`section ${styles['section-recent-posts']}}`}>
                 <h2 className={`section__header`}>Project List</h2>
                 <div className={`section__body ${styles['section-recent-posts__content']}`}>
                     {
-                        postCards.map((p, index) => <PostCard id={p.id} key={index}
+                        mockPosts.map((p, index) => <PostCard id={index} key={index}
                                                               imageUrl={p.imageUrl}
                                                               postDate={p.postDate}
                                                               title={p.title}
@@ -128,10 +63,13 @@ const PostPage = () => {
                 </div>
             </section>
             <section className={`section ${styles['section-post-details']}`}>
-                <p className={'badge'}>post Date</p>
-                <h2>Grid system for better Design User Interface</h2>
-                <img src="https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg" alt="active-post-photo"/>
+                <p className={'badge'}>{response.postDate}</p>
+                <h2>{response.title}</h2>
+                <img src={response.imageUrl} alt="active-post-photo"/>
                 <div className={styles['section-recent-posts__cta']}>
+                    <Markdown remarkPlugins={[gfm]}>
+                        {markdown}
+                    </Markdown>
                     <NewsletterSubscription/>
                 </div>
             </section>
