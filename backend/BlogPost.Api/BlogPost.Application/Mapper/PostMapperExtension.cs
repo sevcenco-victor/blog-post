4cc8c98dfcbdc2e1e661edb80bc85c43689a0090
Victor Sevcenco <sevcenco.dev@gmail.com>
2025-01-04 23:52:12 +0200

    Added DetailedPostResponse to mapping

--- backend/BlogPost.Api/BlogPost.Application/Mapper/PostMapperExtension.cs
+++ backend/BlogPost.Api/BlogPost.Application/Mapper/PostMapperExtension.cs
@@ -5,7 +5,7 @@ namespace BlogPost.Application.Mapper;
 
 public static class PostMapperExtension
 {
-    public static Post ToEntity(this CreatePostRequest post)
+    public static Post ToEntity(this CreatePostRequest post, IEnumerable<Tag> tags, string markdownFileName )
     {
         return new Post()
         {
@@ -13,31 +13,34 @@ public static class PostMapperExtension
             Text = post.Text,
             PostDate = DateOnly.FromDateTime(DateTime.Now.ToUniversalTime()),
             LastEdit = DateTime.Now.ToUniversalTime(),
+            MarkdownFileName = markdownFileName,
+            Tags = tags.ToList(),
             ImageUrl = post.ImageUrl,
-        };
-    }
-    
-    public static CreatePostRequest ToCreatePostDto(this Post post)
-    {
-        return new CreatePostRequest(post.Title, post.Text, post.ImageUrl, post.Tags.Select(t => t.Id));
-    }
-
-    public static Post ToEntity(this PostResponse post)
-    {
-        return new Post()
-        {
-            Id = post.Id,
-            Title = post.Title,
-            Text = post.Text,
-            PostDate = post.PostDate,
-            LastEdit = post.LastEdit,
-            ImageUrl = post.ImageUrl
         };
     }
 
     public static PostResponse ToPostResponseDto(this Post post)
     {
-        return new PostResponse(post.Id, post.Title, post.Text, post.PostDate, post.LastEdit, post.ImageUrl, post.Tags);
+        return new PostResponse(
+            post.Id,
+            post.Title,
+            post.Text,
+            post.PostDate,
+            post.ImageUrl,
+            post.Tags);
+    }
+
+    public static DetailedPostResponse ToDetailedPostResponseDto(this Post post, string markdownFileLink)
+    {
+        return new DetailedPostResponse(
+            post.Id,
+            post.Title,
+            post.Text,
+            post.PostDate,
+            post.LastEdit,
+            post.ImageUrl,
+            markdownFileLink,
+            post.Tags);
     }
 
     public static Post ToEntity(this UpdatePostRequest post)
