10fdc9a7780b72f35db96bc85ddd18db559bf588
Victor Sevcenco <sevcenco.dev@gmail.com>
2025-01-07 03:24:42 +0200

    refactor: code refactor

--- frontend/src/components/NewsletterSubscription/NewsletterSubscription.tsx
+++ frontend/src/components/NewsletterSubscription/NewsletterSubscription.tsx
@@ -1,25 +1,28 @@
-import TextInput from "../Inputs/TextInput/TextInput";
-import Button from "../Buttons/PrimaryButton/Button";
+import {useState} from "react";
+import {Link} from "react-router-dom";
+import TextInput from "../Inputs/TextInput";
+import Button from "../Button";
 import styles from './NewsletterSubscription.module.scss';
 
 
 const NewsletterSubscription = () => {
-    const handleOnChange = (_: any) => {
+    const [userEmail, setUserEmail] = useState("");
 
-    }
     const handleOnClick = () => {
-        console.log("SUBSCRIBE")
+        console.log(`User with email ${userEmail} subscribed`);
     }
 
     return (
-        <div className={styles['section-cta']}>
-            <p className={`badge`}>Newsletter</p>
-            <h2>Stories and interviews</h2>
-            <p>Subscribe to learn about new product features, the latest in technology, solutions, and updates.</p>
-            <div className={styles['section-cta__form']}>
-                <TextInput name={'userEmail'} onChange={handleOnChange} placeholder={'Enter your email'}/>
+        <div className={styles['subscription']}>
+            <p className={`badge ${styles['subscription__badge']}`}>Newsletter</p>
+            <h2 className={`${styles['subscription__header']}`}>Stories and interviews</h2>
+            <p className={`${styles['subscription__description']}`}>Subscribe to learn about new product features, the
+                latest in technology, solutions, and updates.</p>
+            <div className={styles['subscription__form']}>
+                <TextInput name={'userEmail'} onChange={(e) => setUserEmail(e.target.value)}
+                           placeholder={'Enter your email'}/>
                 <Button text={'Subscribe'} onClick={handleOnClick}/>
-                <p>We care about your data in our <a href="#">privacy policy</a></p>
+                <p>We care about your data in our <Link to="/privacy-policy">privacy policy</Link></p>
             </div>
         </div>
     );
