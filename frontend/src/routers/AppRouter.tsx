e67c37093aa2b919930f51db8bbaff8822860182
Victor Sevcenco <sevcenco.dev@gmail.com>
2025-01-07 03:11:10 +0200

    refactor: switch from instant page loading to lazy loading

--- frontend/src/routers/AppRouter.tsx
+++ frontend/src/routers/AppRouter.tsx
@@ -1,32 +1,39 @@
 import {Routes, Route} from "react-router-dom";
-import BlogPage from "../pages/BlogPage/BlogPage";
-import PostPage from "../pages/PostPage/PostPage";
-import AboutPage from "../pages/AboutPage/AboutPage";
-import NewsletterPage from "../pages/NewsletterPage/NewsletterPage";
-import AdminLayout from "../layouts/AdminLayout/AdminLayout";
-import AdminPage from "../pages/(is-logged)/(is-admin)/AdminPage/AdminPage";
-import AdminPostPage from "../pages/(is-logged)/(is-admin)/AdminPostPage/AdminPostPage";
-import AdminTagPage from "../pages/(is-logged)/(is-admin)/AdminTagPage/AdminTagPage";
+import {lazy, Suspense} from "react";
+import Blog from "../pages/Blog/Blog.tsx";
 import AppLayout from "../layouts/AppLayout/AppLayout";
-import ProjectPage from "../pages/ProjectPage/ProjectPage";
+import Loader from "../components/Loader/Loader.tsx";
+
+const ProjectPage = lazy(() => import('../pages/ProjectPage/Project.tsx'));
+const AboutPage = lazy(() => import('../pages/About/About.tsx'));
+const NewsletterPage = lazy(() => import('../pages/NewsletterPage/Newsletter.tsx'));
+const PostPage = lazy(() => import('../pages/Post/Post.tsx'));
+const AdminLayout = lazy(() => import('../layouts/AdminLayout/AdminLayout.tsx'));
+const AdminPage = lazy(() => import('../pages/(is-logged)/(is-admin)/Admin/Admin.tsx'));
+const AdminPostPage = lazy(() => import('../pages/(is-logged)/(is-admin)/AdminPostPage/AdminPost.tsx'));
+const AdminTagPage = lazy(() => import('../pages/(is-logged)/(is-admin)/AdminTagPage/AdminTag.tsx'));
 
 const AppRouter = () => {
     return (
-        <Routes>
-            <Route element={<AppLayout/>}>
-                <Route index element={<BlogPage/>}/>
-                <Route path="post/:postId" element={<PostPage/>}/>
-                <Route path="projects" element={<ProjectPage/>}/>
-                <Route path="about" element={<AboutPage/>}/>
-                <Route path="newsletter" element={<NewsletterPage/>}/>
-                <Route path="admin" element={<AdminLayout/>}>
-                    <Route index element={<AdminPage/>}/>
-                    <Route path="post" element={<AdminPostPage/>}/>
-                    <Route path="tag" element={<AdminTagPage/>}/>
+        <Suspense fallback={<Loader/>}>
+            <Routes>
+                <Route element={<AppLayout/>}>
+                    <Route index element={<Blog/>}/>
+                    <Route path="projects" element={<ProjectPage/>}/>
+                    <Route path="about" element={<AboutPage/>}/>
+                    <Route path="newsletter" element={<NewsletterPage/>}/>
+                    <Route path="post/:postId" element={<PostPage/>}/>
+                    <Route path="admin" element={<AdminLayout/>}>
+                        <Route index element={<AdminPage/>}/>
+                        <Route path="post" element={<AdminPostPage/>}/>
+                        <Route path="tag" element={<AdminTagPage/>}/>
+                    </Route>
                 </Route>
-            </Route>
-        </Routes>
-    );
+            </Routes>
+        </Suspense>
+
+    )
+        ;
 };
 
 export default AppRouter;
