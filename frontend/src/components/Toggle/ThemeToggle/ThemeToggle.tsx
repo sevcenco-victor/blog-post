f31cd7b4a7fefcff6d49ad62bd0ad4a0bdcf556b
Victor Sevcenco <sevcenco.dev@gmail.com>
2025-01-09 01:02:56 +0200

    refactor: switch to CSS module syntax

--- frontend/src/components/Toggle/ThemeToggle/ThemeToggle.tsx
+++ frontend/src/components/Toggle/ThemeToggle/ThemeToggle.tsx
@@ -1,19 +1,18 @@
-import {GoSun} from "react-icons/go";
-import {GoMoon} from "react-icons/go";
-import {useThemeStore} from "../../../stores/useThemeStore.ts";
+import {GoSun, GoMoon} from "react-icons/go";
+import {useThemeStore} from "@/stores/useThemeStore";
 import styles from './ThemeToggle.module.scss';
 
-const ThemeToggle = () => {
-    const isDarkTheme = useThemeStore((state) => state.isDark);
+export const ThemeToggle = () => {
+    const currTheme = useThemeStore((state) => state.theme);
     const toggleTheme = useThemeStore((state) => state.switchTheme);
 
     return (
         <div onClick={toggleTheme}
              role="button"
-             aria-label={`Switch to ${isDarkTheme ? 'light' : 'dark'} theme`}
-             className={`${styles['toggle']} ${isDarkTheme ? styles['toggle--is-active'] : ''}`}>
-            <GoSun className={styles['toggle__sun']}/>
-            <GoMoon className={styles['toggle__moon']}/>
+             aria-label={`Switch to ${currTheme === "dark" ? 'light' : 'dark'} theme`}
+             className={`${styles.toggle} ${currTheme === "dark" ? styles.active : ''}`}>
+            <GoSun className={styles.sun}/>
+            <GoMoon className={styles.moon}/>
         </div>
     );
 };
