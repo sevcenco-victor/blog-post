ad04ce92622a6c12bdeebe9803de9113d35655c9
Victor Sevcenco <sevcenco.dev@gmail.com>
2025-01-09 00:15:30 +0200

    feat: add debounce hook to reduce backend requests

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -3,3 +3,5 @@ export const BASE_URL = "http://localhost:5048/api";
 export const BLOG_PAGE_RECENT_POSTS_NUM = 4;
 export const BLOG_PAGE_VIEW_POST_QTY = 6;
 export const POST_PAGE_LATEST_POSTS_NUM = 12;
+
+export const DEBOUNCE_DELAY = 800;
