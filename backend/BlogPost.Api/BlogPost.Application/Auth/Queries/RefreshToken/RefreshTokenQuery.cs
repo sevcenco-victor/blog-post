7141d12ed4456a36257665e6d383de8fc83e58da
Victor Sevcenco <sevcenco.dev@gmail.com>
2025-01-15 01:06:15 +0200

    refactor: remove unused files

--- backend/BlogPost.Api/BlogPost.Application/Auth/Queries/RefreshToken/RefreshTokenQuery.cs
+++
@@ -1,7 +0,0 @@
-using BlogPost.Application.Contracts.Auth;
-using BlogPost.Domain.Primitives;
-using MediatR;
-
-namespace BlogPost.Application.Auth.Queries.RefreshToken;
-
-public sealed record RefreshTokenQuery(RefreshTokenRequest Data) : IRequest<Result<string>>;
--- backend/BlogPost.Api/BlogPost.Application/Auth/Queries/RefreshToken/RefreshTokenRequest.cs
+++
@@ -1,3 +0,0 @@
-namespace BlogPost.Application.Contracts.Auth;
-
-public record RefreshTokenRequest(int UserId, string RefreshToken);
