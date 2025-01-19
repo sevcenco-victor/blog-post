20e02cc41b0879b12c1b8931df09ac26c0d3169d
Victor Sevcenco <sevcenco.dev@gmail.com>
2025-01-19 23:01:29 +0200

    feat: add getUserPosts method to fetch user-specific posts

--- frontend/src/apis/postRequests.ts
+++ frontend/src/apis/postRequests.ts
@@ -1,40 +1,44 @@
 import axiosInstance from "./axiosInstance.ts";
-import {DetailedPost, Post} from "@/types";
+import {CreatePostRequest, DetailedPost, GetPaginatedPostRequest, Post} from "@/types";
 
-type PostRequest = {
-    title: string,
-    text: string,
-    imageUrl: string,
-    markdownFileContent: string,
-    tagIds: number[]
-}
 
-export const getPosts = async (pageNum = 1, pageSize = 10): Promise<Post[]> => {
+export const getPosts = async ({pageNumber = 1, postNum, title, tagIds}
+                               : GetPaginatedPostRequest): Promise<Post[]> => {
     const response = await axiosInstance.get<Post[]>(`/post/paginated`, {
-        params: {pageSize: pageSize, pageNumber: pageNum},
+        params: {pageSize: postNum, pageNumber: pageNumber, title: title, tagIds: {...tagIds}},
     });
     return response.data;
 }
-export const getPostById = async (id: number): Promise<DetailedPost> => {
+export const getUserPosts = async ({userId, pageNumber = 1, postNum, title, tagIds}
+                                   : GetPaginatedPostRequest): Promise<Post[]> => {
+    const response = await axiosInstance.get<Post[]>(`/post/user/${userId}`, {
+        params: {pageSize: postNum, pageNumber: pageNumber, title: title, tagIds: {...tagIds}},
+    });
+    return response.data;
+}
+
+export const getPostById = async (id: string): Promise<DetailedPost> => {
     const response = await axiosInstance.get<DetailedPost>(`/post/${id}`);
     return response.data;
 }
+
 export const getLatestPosts = async (num: number): Promise<Post[]> => {
     const response = await axiosInstance.get<Post[]>(`/Post/latest`, {
         params: {num: num}
     });
     return response.data;
 }
+
 export const getPostsQuantity = async (): Promise<number> => {
     const response = await axiosInstance.get(`/post/qty`);
     return response.data;
 }
 
-export const addPost = async (body: PostRequest): Promise<number> => {
+export const addPost = async (body: CreatePostRequest): Promise<number> => {
     const response = await axiosInstance.post('/post', body);
     return response.data;
 }
-export const updatePost = async (id: number, body: PostRequest): Promise<void> => {
+export const updatePost = async (id: number, body: CreatePostRequest): Promise<void> => {
     await axiosInstance.put(`/post/${id}`, body);
 }
 export const deletePost = async (id: number): Promise<void> => {
