61e6db13303a62f6f99b851c515b88ab14557eb7
Victor Sevcenco <sevcenco.dev@gmail.com>
2025-01-19 23:18:50 +0200

    refactor: edit About page
    - Scaled the image up to 100%
    - Modified class names to css modules convention
    - Added loading state

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -1,35 +1,40 @@
-import styles from './About.module.scss';
-import Markdown from "react-markdown";
 import {useEffect, useState} from "react";
+import Markdown from "react-markdown";
+import {Loader} from "../../components";
+import styles from './About.module.scss';
 
-const About = () => {
-
+export const About = () => {
     const [content, setContent] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
         fetch('/about.md')
             .then(res => res.text())
             .then(text => setContent(text))
-            .catch(err => console.error('Error fetching markdown file about.md', err));
+            .catch(err => console.error('Error fetching markdown file about.md', err))
+            .finally(() => setIsLoading(false));
     }, [])
 
     return (
-        <div className={`${styles['about-page']}`}>
+        <>
             <section className={`section section-hero`}>
                 <div className={`section-hero__title-wrapper`}>
                     <h1>MURKIZ</h1>
                 </div>
             </section>
-            <section className={`section ${styles['section-about-me']}`}>
-                <img className={styles['section-about-me__image']}
-                     src="https://i.ytimg.com/vi/rvX8cS-v2XM/maxresdefault.jpg" alt="about-me-photo"/>
-                <div className={`section__body ${styles['section-about-me__content']}`}>
-                    <div className={styles['content__item']}>
-                        <Markdown>{content}</Markdown>
-                    </div>
+            <section className={`section ${styles.sectionAboutMe}`}>
+                <img className={styles.img}
+                     src="https://i.ytimg.com/vi/rvX8cS-v2XM/maxresdefault.jpg" alt="about-me-photo" loading={"lazy"}/>
+                <div className={`section__body ${styles.content}`}>
+                    <div className={styles.contentItem}>
+                        {isLoading
+                            ? <Loader/>
+                            : <Markdown>{content}</Markdown>
+                        }</div>
                 </div>
             </section>
-        </div>
+        </>
     );
 };
 
