98a693672c2aa42119a05e7bf77193370d6b7963
Victor Sevcenco <sevcenco.dev@gmail.com>
2025-01-19 23:16:59 +0200

    chore: add new types

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,3 +3,6 @@ export * from './tag.types.ts';
 export * from './theme.types.ts';
 export * from './menuBar.types.ts';
 export * from './input.types.ts';
+export * from './form.types.ts';
+export * from './auth.types.ts';
+export * from './user.types.ts';
